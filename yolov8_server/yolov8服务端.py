96a55856bfcc520e54b124c4946112cebd4c85a6
z <zzw810215@gmail.com>
2023-12-13 15:28:17 +0400

    z

--- yolov8_server/yolov8服务端.py
+++ yolov8_server/yolov8服务端.py
@@ -302,7 +302,7 @@ def run():
                         r_item[9] = 0
 
                 ranking_array.sort(key=lambda x: (x[6]), reverse=True)  # 区域排序数组
-                ranking_array = direction_ranking(ranking_array)  # 再根据区域内位置排序
+                ranking_array = direction_ranking(ranking_array)  # 再根据区域内球位置排序
                 ranking_array.sort(key=lambda x: (x[8]), reverse=True)  # 最后根据圈数排序数组
                 print(ranking_array)
                 con_data = []
